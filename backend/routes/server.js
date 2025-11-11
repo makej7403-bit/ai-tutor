01f8641958f2a49d4dbd549b2632220dc1406b17
makej7403-bit <makej7403@gmail.com>
2025-11-11 21:37:41 +0000

    Create server.js

---
+++ backend/routes/server.js
@@ -0,0 +1,16 @@
+📘 Subjects
+Biology
+Chemistry
+Physics
+Mathematics
+Nursing
+English
+🧠 FullTask AI Tutor
+Ask anything about Biology — AI will answer instantly!
+
+Hi
+Ask AI
+AI Response:
+No response from AI.
+
+© 2025 FullTask AI Tutor | Powered by Akin S. Sokpah | Backend: https://ai-tutor-e5m3.onrender.com
