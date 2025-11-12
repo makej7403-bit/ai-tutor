8b8b10ca9c73d65f673ebbcec82ef6d931573794
makej7403-bit <makej7403@gmail.com>
2025-11-12 00:28:33 +0000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -93,3 +93,13 @@ function App() {
 }
 
 createRoot(document.getElementById("root")).render(<App />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
