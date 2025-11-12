2da0653df9e93345273ac3b14b07d9ecb6a7439b
makej7403-bit <makej7403@gmail.com>
2025-11-12 00:10:55 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    rollupOptions: {
+      external: ['firebase/app', 'firebase/database', 'firebase/auth']
+    }
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/database', 'firebase/auth']
+  }
+})
